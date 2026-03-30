e79d60c30f7d6fb8fc2e5a0df95e8bffbd4f2c90
codewithsaidul <codewithsaidul@gmail.com>
2026-03-30 12:35:21 +0600

    Implement password hashing for user model and update seedSuperAdmin to use plain password

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
 import {
   IAuthProvider,
   IUser,
@@ -6,6 +7,7 @@ import {
   UserRole,
   UserStatus,
 } from "./user.interface";
+import { envVars } from "../../config/env";
 
 const authProviderSchema = new Schema<IAuthProvider>(
   {
@@ -15,7 +17,7 @@ const authProviderSchema = new Schema<IAuthProvider>(
   {
     versionKey: false,
     _id: false,
-  },
+  }
 );
 
 const userSchema = new Schema<IUser, UserModel>(
@@ -23,6 +25,7 @@ const userSchema = new Schema<IUser, UserModel>(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, select: 0 },
+
 
     providers: [authProviderSchema],
 
@@ -36,14 +39,33 @@ const userSchema = new Schema<IUser, UserModel>(
       enum: [...Object.values(UserStatus)],
       default: UserStatus.PENDING,
     },
-
+    
     isDeleted: { type: Boolean, default: false },
   },
-  { timestamps: true, versionKey: false },
+  { timestamps: true, versionKey: false }
 );
+
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password") && this.password) {
+    this.password = await bcrypt.hash(
+      this.password,
+      Number(envVars.BCRYPT_SALT_ROUND)
+    );
+  }
+  next();
+});
 
 userSchema.statics.isUserExistsByEmail = async function (email: string) {
   return await User.findOne({ email }).select("+password");
+};
+
+userSchema.statics.isPasswordMatched = async function (
+  plainTextPassword,
+  hashedPassword
+) {
+  if (!hashedPassword) return false;
+  return await bcrypt.compare(plainTextPassword, hashedPassword);
 };
 
 export const User = model<IUser, UserModel>("User", userSchema);
--- src/app/utils/seedSuperAdmin.ts
+++ src/app/utils/seedSuperAdmin.ts
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
 import { envVars } from "../config/env";
-import bcrypt from "bcryptjs";
+import {
+  IAuthProvider,
+  IUser,
+  UserRole,
+  UserStatus,
+} from "../modules/user/user.interface";
 import { User } from "../modules/user/user.model";
-import { IAuthProvider, IUser, UserRole, UserStatus } from "../modules/user/user.interface";
-
 
 export const seedSuperAdmin = async () => {
   try {
@@ -14,35 +17,24 @@ export const seedSuperAdmin = async () => {
       return null;
     }
 
-      console.log("🚀 ~ seedSuperAdmin ~ process.env.SUPER_ADMIN_PASSWORD:", process.env.SUPER_ADMIN_PASSWORD)
-
-
-    const hashPassword = await bcrypt.hash(
-      process.env.SUPER_ADMIN_PASSWORD as string,
-      parseInt(envVars.BCRYPT_SALT_ROUND as string)
-    );
-
     const authProvider: IAuthProvider = {
       provider: "credentials",
       providerId: envVars.SUPER_ADMIN_EMAIL,
     };
 
-
     const superAdminInfo: IUser = {
-        name: "Inventory Management - Super Admin",
-        email: envVars.SUPER_ADMIN_EMAIL,
-        password: hashPassword,
-        role: UserRole.SUPERADMIN,
-        status: UserStatus.ACTIVE,
-        providers: [authProvider],
-        isDeleted: false,
-    }
-
+      name: "Inventory Management - Super Admin",
+      email: envVars.SUPER_ADMIN_EMAIL,
+      password: process.env.SUPER_ADMIN_PASSWORD as string,
+      role: UserRole.SUPERADMIN,
+      status: UserStatus.ACTIVE,
+      providers: [authProvider],
+      isDeleted: false,
+    };
 
     await User.create(superAdminInfo);
 
     console.log("Super Admin seeded successfully.");
-
   } catch (error) {
     console.log(error);
   }
